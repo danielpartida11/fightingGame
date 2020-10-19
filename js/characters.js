4b7971ef016f532f480eeabdcf95388bad559bc3
unknown <danielpartida32@hotmail.com>
2020-10-19 23:38:01 +0200

    archivos js creados y escenario de seleccion de personaje creado

---
+++ js/characters.js
@@ -0,0 +1,23 @@
+let luchador1 = {
+    nombre: "marco",
+    vida: 100,
+    ataque: 45
+}
+
+let luchador2 = {
+    nombre: "mario",
+    vida: 150,
+    ataque: 20
+}
+
+let luchador3 = {
+    nombre: "sonic",
+    vida: 90,
+    ataque: 55
+}
+
+let luchador4 = {
+    nombre: "steve",
+    vida: 300,
+    ataque: 10
+}
